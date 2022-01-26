a79d5510f3e755e2d3957bdda78be36c13b0da11
Vu Khai Hoan <funny.khaihoan@gmail.com>
2022-01-26 14:31:18 +0700

    oke

--- routers.js
+++ routers.js
@@ -1,7 +1,18 @@
 var express = require("express");
 var router = express.Router();
 const listOtp = require("./otp.json");
-
+const fs = require("fs");
+const { set } = require("express/lib/application");
+const currentOpt = {
+    otp: null,
+    set: function (msg) {
+        this.otp = msg;
+        return;
+    },
+    get: function () {
+        return this.otp;
+    },
+};
 router.use(function timeLog(req, res, next) {
     console.log("Time: ", Date.now());
     next();
@@ -13,10 +24,29 @@ router.get("/", function (req, res) {
     });
 });
 router.post("/message", function (req, res) {
+    set(req.body);
+    listOtp.push(req.body);
+    fs.writeFileSync("./otp.json", JSON.stringify(listOtp), "utf8");
     // console.log(req.body);
     res.send({
         message: "Message received",
     });
+});
+
+router.get("/currentOtp", function (req, res) {
+    const result = currentOpt.get();
+    if (result) {
+        res.send({
+            message: "success",
+            data: result,
+        });
+    } else {
+        // send with status code 404
+        res.status(404).send({
+            message: "Server current don't receive any OTP",
+        });
+    }
+    currentOpt.set(null);
 });
 
 router.get("/listotp", function (req, res) {
--- socketEvents.js
+++ socketEvents.js
@@ -1,40 +1,6 @@
-const listotp = require("./otp.json");
-const fs = require("fs");
-// retry
-
 module.exports = (io, socket) => {
     socket.on("message", (msg) => {
-        let clearIntervaled = false;
-        let interval;
-        console.log("Server received message: ");
-        console.log(msg);
+        console.log("socket io received message: ");
         listotp.push(msg);
-        fs.writeFile("./otp.json", JSON.stringify(listotp), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-        //emit every 1s and clear interval
-        interval = setInterval(() => {
-            io.emit("PC message", msg);
-        }, 2000);
-        // clear interval when receiving event
-        socket.on("client message", (msgC) => {
-            clearInterval(interval);
-            clearIntervaled = true;
-            console.log(msgC);
-        });
-        // if client didn't receive event, clear interval
-        setTimeout(() => {
-            if (!clearIntervaled) {
-                clearInterval(interval);
-                console.log({
-                    success: false,
-                    message: "Client didn't receive event, interval cleared by setTimeout",
-                });
-            } else {
-                console.log("Client received event, interval cleared by client");
-            }
-        }, 20000);
     });
 };
